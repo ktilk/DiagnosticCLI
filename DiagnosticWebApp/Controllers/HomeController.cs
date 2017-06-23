b6a2b9a7dafa8bc58470f0327c2b7798d572c95f
Kaspar Tilk <kaspar.tilk@itcollege.ee>
2017-06-23 05:23:31 +0300

    web app csv parse

--- DiagnosticWebApp/Controllers/HomeController.cs
+++ DiagnosticWebApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using DiagnosticWebApp.Models;
 
 namespace DiagnosticWebApp.Controllers
 {
@@ -16,13 +17,37 @@ namespace DiagnosticWebApp.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult InsertData(HttpPostedFileBase data)
+        public ActionResult InsertData(HttpPostedFileBase dataFile)
         {
-            var csvReader = new StreamReader(data.InputStream);
-            var line = csvReader.ReadLine();
-            var values = line.Split();
-            return Index();
+            var diseases = new List<Disease>();
+            var symptoms = new List<Symptom>();
+            var csvReader = new StreamReader(dataFile.InputStream);
+            while (!csvReader.EndOfStream)
+            {
+                var line = csvReader.ReadLine();
+                var disease = new Disease();
+                var words = line.Split().Select(s => s.Trim()).ToArray();
+                disease.DiseaseName = words[0];
+                for (var i = 1; i < words.Length; i++)
+                {
+                    words[i] = words[i].Trim();
+                    var symp = symptoms.FirstOrDefault(s => s.SymptomName == words[i]);
+                    if (symp == null) // create symptom if does not exist
+                    {
+                        symp = new Symptom() { SymptomName = words[i] };
+                        symptoms.Add(symp);
+                    }
+                    disease.Symptoms.Add(symp);
+                }
+                diseases.Add(disease);
+            }
+            return Index(diseases, symptoms);
         }
 
+        private ActionResult Index(List<Disease> diseases, List<Symptom> symptoms)
+        {
+            //TODO tee andmetega maagiat ja pane lehel kuvama
+            throw new NotImplementedException();
+        }
     }
 }
